ed9a5fe6debef7896cbf7034c80720a2f71fe59c
nimivera <etimv0524@gmail.com@example.com>
2025-07-09 23:53:58 +0000

    Created Three Specific Components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,4 @@
+import UserProfile from './UserProfile';
 import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
@@ -40,6 +41,7 @@ function App() {
       <MainContent />
       <Footer />
     </div>
+    <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
     </>
   )
 }
