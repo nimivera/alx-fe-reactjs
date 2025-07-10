60665de3af2cae1310ec8b90465ab2bd0acc1a0b
nimivera <etimv0524@gmail.com@example.com>
2025-07-10 23:16:43 +0000

    Updated import WelcomeMessage

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,7 +3,7 @@ import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
 
-import WelcomeMessage from './WelcomeMessage';
+import WelcomeMessage from './components/WelcomeMessage';
 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
