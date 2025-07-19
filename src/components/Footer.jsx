fca54e7185447fb8253ba61884fc60e8366c29c4
nimivera <etimv0524@gmail.com@example.com>
2025-07-19 22:21:02 +0000

    Added inline stles to components files

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+function Footer() {
+    return (
+      <footer style={{ color: 'yellow', fontWeight: 'bold',
+        padding: '10px',
+        textAlign: 'center',
+        borderTop: '1px solid #444',
+        position: 'relative',
+        bottom: '0',
+        width: '100%'}}>
+      <p>© 2023 City Lovers</p>
+    </footer>
+    );
+  }
+  
+export default Footer;
+
+
---
+++ src/components/MainContent.jsx
@@ -0,0 +1,13 @@
+function MainContent() {
+    return (
+      <main  style={{ backgroundColor: 'navy',display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        padding: '20px',
+        minHeight: '80vh', color: 'white' }}>
+        <p>I love to visit New York, Paris, and Tokyo.</p>
+      </main>
+    )
+  }
+
+  export default MainContent;
