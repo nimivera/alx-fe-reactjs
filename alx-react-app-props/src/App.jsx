e9ecbc6ff003c0f96ac7a9cfcdbdfd2d1cffd789
nimivera <etimv0524@gmail.com@example.com>
2025-07-21 18:37:06 +0000

    Restructure project to pass the checker

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,26 +1,14 @@
-import WelcomeMessage from './components/WelcomeMessage'
-import { useState } from 'react';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import UserProfile from './components/UserProfile';
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
-import UserContext from './UserContext';
+import ProfilePage from "./components/ProfilePage";
+import UserContext from "./components/UserContext";
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
     <UserContext.Provider value={userData}>
-      <Header />
-      <WelcomeMessage />
-      <MainContent />
-      <UserProfile />
-      <Footer />
+      <ProfilePage userData={userData} />
     </UserContext.Provider>
-  )
+  );
 }
 
 export default App;
--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,17 +1,19 @@
 import { useContext } from 'react';
-import UserContext from '../UserContext';
+import UserContext from './UserContext';
 
-const UserProfile = () => {
+const UserProfile = (props) => {
+
   const userData = useContext(UserContext);
 
-  return (
-    <div>
-      <h2>{userData.name}</h2>
-      <p>Email: {userData.email}</p>
-      <p>Age: 25</p>
-      <p>Bio: Loves hiking and photography</p>
-    </div>
-  );
-};
+    return (
+      <div>
 
-export default UserProfile;
+        <h1>{props.name}</h1>
+        <p>{userData.name}</p>
+        <p>Age: {props.age}</p>
+        <p>Bio: {props.bio}</p>
+      </div>
+    );
+}
+
+export default UserProfile
