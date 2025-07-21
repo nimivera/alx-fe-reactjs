67aaa8ae01e7d27243cdf580d1a1e813cc043fa9
nimivera <etimv0524@gmail.com@example.com>
2025-07-21 16:57:48 +0000

    Updating App.jsx file

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -7,20 +7,20 @@ import UserProfile from './components/UserProfile';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserContext from './UserContext';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
-    <>
-    <Header />
-     <WelcomeMessage />
-     <MainContent />
-     <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-     <Footer />
-
-    </>
+    <UserContext.Provider value={userData}>
+      <Header />
+      <WelcomeMessage />
+      <MainContent />
+      <UserProfile />
+      <Footer />
+    </UserContext.Provider>
   )
 }
 
-export default App
+export default App;
