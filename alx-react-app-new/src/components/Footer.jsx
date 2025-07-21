1b234f5e73b8b5039f8dbe8249e13894785fc31f
nimivera <etimv0524@gmail.com@example.com>
2025-07-21 13:21:00 +0000

    Update inline styles

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+  return (
+  <footer style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center', padding: '10px', position: 'fixed', bottom: '0', width: '100%' }}>
+
+    <p>© 2023 City Lovers</p>
+  </footer>
+  );
+}
+
+export default Footer;
+
+
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+    <main style={{ display: 'flex', justifyContent: 'center', padding: '20px', minHeight: '80vh' }}>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
