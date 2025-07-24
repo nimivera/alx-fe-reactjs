42ff8d39591334501aec841e4a4dbf829173a1a9
nimivera <etimv0524@gmail.com@example.com>
2025-07-24 23:59:42 +0000

    Updatting files

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,5 +1,5 @@
-import RecipeList  from './components/RecipeList'
-import  AddRecipeForm from './components/AddRecipeForm'
+import  AddRecipeForm from './components/AddRecipeForm.jsx'
+import RecipeList from './components/RecipeList.jsx'
 
 function App() {
 
