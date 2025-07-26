667e773bb0aa181c3aa6772f6774ea6922725caa
nimivera <etimv0524@gmail.com@example.com>
2025-07-26 14:22:44 +0000

    Added Link import from react-router-dom in RecipeList

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,5 +1,6 @@
 // RecipeList component
 import { useRecipeStore } from './recipeStore';
+import { Link } from 'react-router-dom';
 
 const RecipeList = () => {
   const filteredRecipes = useRecipeStore(state => state.filteredRecipes);
