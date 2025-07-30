f06aa459632b4b0daf8617c84b13f6cb89c664b6
nimivera <etimv0524@gmail.com@example.com>
2025-07-30 14:42:36 +0000

    Updated components to use githubService

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import axios from 'axios';
+import { getUser } from '../services/githubService';
 
 function Search() {
   const [username, setUsername] = useState('');
@@ -8,32 +8,12 @@ function Search() {
   const handleSearch = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.get(`https://api.github.com/users/${username}`);
-      setUserData(response.data);
+      const user = await getUser(username);
+      setUserData(user);
     } catch (error) {
       console.error(error);
     }
   };
 
-  return (
-    <div>
-      <form onSubmit={handleSearch}>
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          placeholder="Search for a GitHub user"
-        />
-        <button type="submit">Search</button>
-      </form>
-      {userData && (
-        <div>
-          <h2>{userData.login}</h2>
-          <img src={userData.avatar_url} alt={userData.login} />
-        </div>
-      )}
-    </div>
-  );
+  // ...
 }
-
-export default Search;
