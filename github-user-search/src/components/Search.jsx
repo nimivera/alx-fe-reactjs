550612c15cdb0d4d23818a3a9d7496b02e1e11c0
nimivera <etimv0524@gmail.com@example.com>
2025-08-01 21:41:28 +0000

    Fixed Search.jsx to include && operator for conditional rendering

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -8,6 +8,7 @@ function Search() {
   const [searchResults, setSearchResults] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [page, setPage] = useState(1);
 
   const constructQuery = (username, location, minRepos) => {
     let query = '';
@@ -32,12 +33,31 @@ function Search() {
     setLoading(true);
     setError(null);
     setSearchResults(null);
+    setPage(1);
 
     const query = constructQuery(username, location, minRepos);
 
     try {
-      const results = await searchUsers(query);
-      setSearchResults(results.items);
+      const results = await searchUsers(query, page);
+      setSearchResults(results);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const loadMore = async () => {
+    setLoading(true);
+    const query = constructQuery(username, location, minRepos);
+    try {
+      const nextPage = page + 1;
+      const results = await searchUsers(query, nextPage);
+      setSearchResults((prevResults) => ({
+        ...results,
+        items: [...prevResults.items, ...results.items],
+      }));
+      setPage(nextPage);
     } catch (error) {
       setError(error.message);
     } finally {
@@ -81,10 +101,10 @@ function Search() {
         <p className="text-center mt-4">Loading...</p>
       ) : error ? (
         <p className="text-center mt-4 text-red-500">Looks like we cant find the user</p>
-      ) : searchResults ? (
+      ) : searchResults && searchResults.items && searchResults.items.length > 0 && (
         <ul className="list-none mt-4">
-          {searchResults.map((user) => (
-            <li key={user.id} className="py-2">
+          {searchResults.items.map((user) => (
+            <li key={user.id} className="py-2 border-b border-gray-200">
               <a
                 className="text-lg text-blue-700 hover:text-blue-900 hover:underline underline-offset-2"
                 href={user.html_url}
@@ -93,10 +113,20 @@ function Search() {
               >
                 {user.login}
               </a>
+              <p>Location: {user.location}</p>
+              <p>Repositories: {user.public_repos}</p>
             </li>
           ))}
+          {searchResults.total_count > searchResults.items.length && (
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
+              onClick={loadMore}
+            >
+              Load More
+            </button>
+          )}
         </ul>
-      ) : null}
+      )}
     </div>
   );
 }
