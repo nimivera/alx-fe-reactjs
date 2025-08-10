a2182010d24764642722515c5ee0a4469ad02822
nimivera <etimv0524@gmail.com@example.com>
2025-08-10 12:11:47 +0000

    Updated Nav.jsx file

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Home';
-import About from './About';
+import About from './components/About';
 import Services from './Services';
-import Contact from './Contact';
-import Navbar from './Navbar';
+import Contact from './components/Contact';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
